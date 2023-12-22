9b6103376fc6e6425164a6c06d97f603b6516d40
Eduard Sagnier Gonzalez <eduard.sagnier@enti.cat>
2023-12-22 13:57:58 +0100

    4 matrices hechas con agua

--- clase/Source.cpp
+++ clase/Source.cpp
@@ -8,25 +8,53 @@
 #include <locale.h> // Libreria que contiene la funcion setlocale
 using namespace std; //Permite no utilizar std::
 
+#define NUM_FILAS 10
+#define NUM_COLUMNAS 10
 
 void main()
 {
 	setlocale(LC_ALL, ""); // Uso de la funcion setlocale
-	
-	char campo[10][10];
+		
+	char campo_jugador_1[NUM_FILAS][NUM_COLUMNAS], campo_jugador_2[NUM_FILAS][NUM_COLUMNAS], disparo_jugador_1[NUM_FILAS][NUM_COLUMNAS],disparo_jugador_2[NUM_FILAS][NUM_COLUMNAS];
 
 	string jugador[2];
 	
 	
-	for (int i = 0; i < 10; i++)
-	{
-		campo[i][i] = '~';
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+			campo_jugador_1[i][j] = '~';
+			campo_jugador_2[i][j] = '~';
+			disparo_jugador_1[i][j] = '~';
+			disparo_jugador_2[i][j] = '~';
+		}
+	}
+
+
+	cout << "Campo 1" << endl;
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+			cout << campo_jugador_1[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "Campo 2" << endl;
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+			cout << campo_jugador_2[i][j] << " ";
+		}
+		cout << endl;
 	}
 	
-	cout << campo[1][2] ;
+	srand(std::time(NULL));
 
+
+	rand() % 3 + 4;
+
+
+		
 	cout << "Hundir la flota" << endl;
-	cout << "By Edu" << endl;
+	cout << "By @Edusagnier" << endl;
 	cout << "" << endl;
 
 	cout << "Introduce tu nombre Jugador 1: ";
@@ -43,12 +71,6 @@ void main()
 	cout << "" << endl;
 
 	
-
-
-	cout << "Barcos" << endl;
-	cout << "4 Barcos de 1 cuadrado:" << endl;
-	cout << "3 Barcos de 2 cuadrados:" << endl;
-	cout << "2 Barcos de 3 cuadrados:" << endl;
-	cout << "1 Barco de 4 cuadrados" << endl;
+	
 	
 }
